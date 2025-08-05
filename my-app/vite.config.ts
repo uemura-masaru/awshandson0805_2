45d7f83d0b7b9619d064c0150a504c1bc7fd3eb3
uemura-masaru <uemura.masaru@tis.co.jp>
2025-08-05 01:50:53 +0000

    Initial Commit

---
+++ my-app/vite.config.ts
@@ -0,0 +1,15 @@
+/// <reference types='vite/client' />
+
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+    base: '/my-app',
+  server: {
+    allowedHosts: true,
+    host: true,
+    port: 8081,
+  },
+})
